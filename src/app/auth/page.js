a278f921f9147c88975cf5cfd0b573aaae0bb01d
Folayimi <ridwanfolayimi@gmail.com>
2023-07-26 22:08:58 -0700

    started something

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <h1>WELCOME TO THE CREOS LAB</h1>
+      <h1>CREATE AN ACCOUNT HERE</h1>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,57 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  purge: {
+    content: [
+      "./src/pages/**/*.{js,jsx,ts,tsx}",
+      "./src/components/**/*.{js,jsx,ts,tsx}",
+    ],
+  },
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary1: "#FFFFFF",
+        primary2: "#8C8C8C",
+        primary3: "#3A3A3A",
+        primary4: "#9C9C9C",
+        primary7: "#CB0000",
+        primary8: "#0C4DF3",
+        primary9: "#5E00D6",
+        primary10: "#0D0E15",
+        secondary1: {
+          100: "#1B4ED2 50%",
+          200: "rgba(181, 65, 162, 0.93) 100%",
+        },
+        secondary2: {
+          100: "#EA5718 0%",
+          200: "#C70F0F 100%",
+        },
+        secondary3: {
+          100: "#056ECF 0%",
+          200: "#180878 65%",
+        },
+        secondary4: {
+          100: "#030D21 0%",
+          200: "rgba(0, 1, 2, 0.94) 65%",
+        },
+        secondary5: {
+          200: "#5E00D6 0%",
+          100: "rgba(0, 25, 250, 0.24) 100%",
+        }
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        'lt': { 'max' : '500px'},
+        'sm': { 'max': '600px' },
+        'sm1': { 'max': "768px" },
+        'md': { 'max': "1100px" },
+        'md1': { 'max': "850px"},
+        'lg': { 'min': "2300px" },
+        'xl': { 'max': "1280px" },
+        "2xl": { 'max': "1536px" },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
