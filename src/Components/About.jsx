9616db413a0040f683ae983af76208361632e7c0
Folayimi <ridwanfolayimi@gmail.com>
2023-08-22 10:28:51 -0700

    Made some changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,157 +1,233 @@
-import Button from '@/AtomicComponents/Button'
-import Footer from '@/AtomicComponents/Footer'
-import NavBar from '@/AtomicComponents/NavBar'
-import PictureTextDivider from '@/AtomicComponents/PictureTextDivider'
-import React from 'react'
-import { FaArrowRight } from 'react-icons/fa'
+import Button from "@/AtomicComponents/Button";
+import Footer from "@/AtomicComponents/Footer";
+import NavBar from "@/AtomicComponents/NavBar";
+import PictureTextDivider from "@/AtomicComponents/PictureTextDivider";
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const About = () => {
   return (
     <>
-        <NavBar active={1}/>
+      <NavBar active={1} />
 
-          <PictureTextDivider imageUrl={"/about-1.jpeg"} bgColor="bg-primary6" imageClassNames={"w-[100%] rounded-[1.5em] margin-auto object-cover h-[100%]"} imageContainerClassNames={"h-[674px] sm:h-[500px] p-[10px] bg-white rounded-[1.8em]"} imageFirst={true}>
-            <div className='flex flex-col gap-[1em] sm:gap-[0.5em] leading-[1.7em] sm:text-[#333]'>
-              <p className="text-[1em] font-[600]">
-                About Us
-              </p>
+      <PictureTextDivider
+        imageUrl={"/about-1.jpeg"}
+        bgColor="bg-primary6"
+        imageClassNames={
+          "w-[100%] rounded-[1.5em] margin-auto object-cover h-[100%]"
+        }
+        imageContainerClassNames={
+          "h-[674px] sm:h-[500px] p-[10px] bg-white rounded-[1.8em]"
+        }
+        imageFirst={true}
+      >
+        <div className="flex flex-col gap-[1em] sm:gap-[0.5em] leading-[1.7em] sm:text-[#333]">
+          <p className="text-[1em] font-[600]">About Us</p>
 
-              <h3 className="text-[1.6em] sm:text-[1.8rem] font-[700] sm:font-[800] text-primary1 sm:leading-[1.5em]">
-                Unleashing Curiosity and Empowering Young Minds
-              </h3>
+          <h3 className="text-[1.6em] sm:text-[1.8rem] font-[700] sm:font-[800] text-primary1 sm:leading-[1.5em]">
+            Unleashing Curiosity and Empowering Young Minds
+          </h3>
 
-              <p className="text-[1.0em] pt-[1em]">
-              Welcome to CuriousKidz, an innovative educational method that bridges the gap between conventional education and the exciting world of STEAM (Science, Technology, Engineering, Arts, and Mathematics).
-              </p>
+          <p className="text-[1.0em] pt-[1em]">
+            Welcome to CuriousKidz, an innovative educational method that
+            bridges the gap between conventional education and the exciting
+            world of STEAM (Science, Technology, Engineering, Arts, and
+            Mathematics).
+          </p>
 
-              <p className="text-[1.0em] pt-[1em]">
-                At CuriousKidz, we believe that every young mind holds boundless potential, waiting to be unleashed through curiosity, creativity, and problem-solving skills.
-              </p>
+          <p className="text-[1.0em] pt-[1em]">
+            At CuriousKidz, we believe that every young mind holds boundless
+            potential, waiting to be unleashed through curiosity, creativity,
+            and problem-solving skills.
+          </p>
 
-              <div className='flexsm gap-3 text-[0.8em] font-[700] sm:items-start pt-[1em] sm:w-full flex-wrap'>
-                <Button className={"flex items-center gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 flex-grow justify-center"}>
-                  Enroll now <FaArrowRight />
-                </Button>
+          <div className="flexsm gap-3 text-[0.8em] font-[700] sm:items-start pt-[1em] sm:w-full flex-wrap">
+            <Button
+              className={
+                "flex items-center gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 flex-grow justify-center"
+              }
+            >
+              Enroll now <FaArrowRight />
+            </Button>
 
-                <Button className={"flex items-center gap-1 rounded-full px-[2.5em] py-[0.9em] text-primary1 bg-transparent border-primary1 border-2 flex-grow justify-center"}>
-                  Login as a guest
-                </Button>
-              </div>
-            </div>
-          </PictureTextDivider>
-
-          <PictureTextDivider imageUrl={"/about-2.png"} bgColor={"bg-transparent"} imageFirst={false}>
-              <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] sm:pt-[2em]">
-              <h3 className="text-primary1 font-[700] text-[1.6em]">
-                Our Vision
-              </h3>
-
-              <p className='text-0.9em'>
-                Our vision is to empower young learners in Nigeria and beyond with the tools they need to excel in rare and high-paying disciplines.
-              </p>
-
-              <p className='text-0.9em'>
-                The STEAM program at CuriousKidz is meticulously designed to engage young minds in conventional education settings, while simultaneously igniting a passion for innovation and exploration.
-              </p>
-              </div>
-          </PictureTextDivider>
-
-          <div className='bg-[#FAFAFA]'>
-          <PictureTextDivider imageUrl={"/about-3.png"} bgColor={"bg-transparent"} imageFirst={true}>
-              <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]">
-              <h3 className="text-primary1 font-[700] text-[1.6em]">
-                Overcoming Chalenges
-              </h3>
-
-              <p className='text-0.9em'>
-              In Nigeria, we recognize two major factors that hinder the progress of the STEM/STEAM program: cost and limited engaging environments. Creating enriching experiences like acoustically friendly music labs, fully tooled technical workshops, and 3D printers can be expensive. 
-              </p>
-
-              <p className='text-0.9em'>
-                  These resources are often readily available in developed societies, making it easier for children in those regions to explore fields like aerospace, astronomy, robotics, computer programming, and systems theory.
-              </p>
-
-              <Button className={"flex items-center w-fit gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 sm:w-full sm:justify-center"}>
-                  Enroll now <FaArrowRight />
-                </Button>
-              </div>
-          </PictureTextDivider>
+            <Button
+              className={
+                "flex items-center gap-1 rounded-full px-[2.5em] py-[0.9em] text-primary1 bg-transparent border-primary1 border-2 flex-grow justify-center"
+              }
+            >
+              Login as a guest
+            </Button>
           </div>
+        </div>
+      </PictureTextDivider>
 
-          <PictureTextDivider imageUrl={"/about-4.png"} bgColor={"bg-transparent"} imageFirst={false}>
-              <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]">
-              <h3 className="text-primary1 font-[700] text-[1.6em] leading-[1.5em]">
-                Feeling Curiousity, Curbing Boredom
-              </h3>
+      <PictureTextDivider
+        imageUrl={"/about-2.png"}
+        bgColor={"bg-transparent"}
+        imageFirst={false}
+      >
+        <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] sm:pt-[2em]">
+          <h3 className="text-primary1 font-[700] text-[1.6em]">Our Vision</h3>
 
-              <p className='text-0.9em'>
-                  We understand that traditional instructional methods can leave young minds feeling bored and disconnected from their studies. To address this, CuriousKidz leverages mediums like TV, movies, and the internet – platforms that naturally captivate children's attention. 
-              </p>
+          <p className="text-0.9em">
+            Our vision is to empower young learners in Nigeria and beyond with
+            the tools they need to excel in rare and high-paying disciplines.
+          </p>
 
-              <p className='text-0.9em'>
-                  We firmly believe that by incorporating these mediums into the learning process, we can create an engaging, research-based, peer-to-peer learning environment.
-              </p>
-              </div>
-          </PictureTextDivider>
+          <p className="text-0.9em">
+            The STEAM program at CuriousKidz is meticulously designed to engage
+            young minds in conventional education settings, while simultaneously
+            igniting a passion for innovation and exploration.
+          </p>
+        </div>
+      </PictureTextDivider>
 
-          <div className='bg-[#FAFAFA]'>
-          <PictureTextDivider imageUrl={"/about-5.png"} imageFirst={true}>
-            <div className='flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]'>
-              <h3 className="text-primary1 font-[700] text-[1.6em] leading-[1.5em]">
-                Nuturing a Passion for Knowledge
-              </h3>
+      <div className="bg-[#FAFAFA]">
+        <PictureTextDivider
+          imageUrl={"/about-3.png"}
+          bgColor={"bg-transparent"}
+          imageFirst={true}
+        >
+          <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]">
+            <h3 className="text-primary1 font-[700] text-[1.6em]">
+              Overcoming Chalenges
+            </h3>
 
-              <p className="text-[1.0em]">
-                Our platform is specifically designed to drive creativity, encourage self-driven research, and instill a thirst for advanced sciences and arts.
-              </p>
+            <p className="text-0.9em">
+              In Nigeria, we recognize two major factors that hinder the
+              progress of the STEM/STEAM program: cost and limited engaging
+              environments. Creating enriching experiences like acoustically
+              friendly music labs, fully tooled technical workshops, and 3D
+              printers can be expensive.
+            </p>
 
-              <p className="text-[1.0em]">
-                By seamlessly integrating real-world applications and purpose-driven knowledge from conventional subjects, we create a dynamic educational experience that empowers students to flourish academically and personally.
-              </p>
+            <p className="text-0.9em">
+              These resources are often readily available in developed
+              societies, making it easier for children in those regions to
+              explore fields like aerospace, astronomy, robotics, computer
+              programming, and systems theory.
+            </p>
 
-            </div>
-          </PictureTextDivider>
-
-          <PictureTextDivider imageUrl={"/about-6.png"} imageFirst={false}>
-            <div className='flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]'>
-              <h3 className="text-primary1 font-[700] text-[1.6em]">
-                Our Commitment
-              </h3>
-
-              <p className="text-[1.0em]">
-              At CuriousKidz, we are committed to cultivating a generation of young learners who possess a genuine love for knowledge. By providing an entertaining and insightful approach to learning, we aim to inspire students to reach for the stars and pursue advanced disciplines in higher education.
-              </p>
-
-              <p className="text-[1.0em]">
-              Join us on this thrilling journey of discovery and growth. Together, let's unlock the potential within each young mind, nurturing curious minds that will shape the future of our world.
-              </p>
-              <div className=' block flexsm gap-3 text-[0.8em] font-[700] sm:items-start pt-[1em] sm:w-full flex-wrap sm:hidden'>
-                <Button className={"flex items-center gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 flex-grow justify-center"}>
-                  Enroll now <FaArrowRight />
-                </Button>
-
-                <Button className={"flex items-center gap-1 rounded-full px-[2.5em] py-[0.9em] text-primary1 bg-transparent border-primary1 border-2 flex-grow justify-center"}>
-                  Login as a guest
-                </Button>
-              </div>              
-            </div>
-          </PictureTextDivider>  
-          <div className="hidden sm:block w-full sm:px-xpadding">
-          <div className='flexsm gap-3 text-[0.8em] font-[700] sm:items-start pt-[1em] sm:w-full flex-wrap bg-[#FAFAFA] pb-[5em]'>
-                <Button className={"flex items-center gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 flex-grow justify-center"}>
-                  Enroll now <FaArrowRight />
-                </Button>
-
-                <Button className={"flex items-center gap-1 rounded-full px-[2.5em] py-[0.9em] text-primary1 bg-transparent border-primary1 border-2 flex-grow justify-center"}>
-                  Login as a guest
-                </Button>
-          </div> 
-          </div>                 
+            <Button
+              className={
+                "flex items-center w-fit gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 sm:w-full sm:justify-center"
+              }
+            >
+              Enroll now <FaArrowRight />
+            </Button>
           </div>
+        </PictureTextDivider>
+      </div>
 
-        <Footer />
+      <PictureTextDivider
+        imageUrl={"/about-4.png"}
+        bgColor={"bg-transparent"}
+        imageFirst={false}
+      >
+        <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]">
+          <h3 className="text-primary1 font-[700] text-[1.6em] leading-[1.5em]">
+            Feeling Curiousity, Curbing Boredom
+          </h3>
+
+          <p className="text-0.9em">
+            We understand that traditional instructional methods can leave young
+            minds feeling bored and disconnected from their studies. To address
+            this, CuriousKidz leverages mediums like TV, movies, and the
+            internet – platforms that naturally captivate children's attention.
+          </p>
+
+          <p className="text-0.9em">
+            We firmly believe that by incorporating these mediums into the
+            learning process, we can create an engaging, research-based,
+            peer-to-peer learning environment.
+          </p>
+        </div>
+      </PictureTextDivider>
+
+      <div className="bg-[#FAFAFA]">
+        <PictureTextDivider imageUrl={"/about-5.png"} imageFirst={true}>
+          <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]">
+            <h3 className="text-primary1 font-[700] text-[1.6em] leading-[1.5em]">
+              Nuturing a Passion for Knowledge
+            </h3>
+
+            <p className="text-[1.0em]">
+              Our platform is specifically designed to drive creativity,
+              encourage self-driven research, and instill a thirst for advanced
+              sciences and arts.
+            </p>
+
+            <p className="text-[1.0em]">
+              By seamlessly integrating real-world applications and
+              purpose-driven knowledge from conventional subjects, we create a
+              dynamic educational experience that empowers students to flourish
+              academically and personally.
+            </p>
+          </div>
+        </PictureTextDivider>
+
+        <PictureTextDivider imageUrl={"/about-6.png"} imageFirst={false}>
+          <div className="flex flex-col gap-[1.5em] leading-[1.7em] sm:text-[#333] pt-[2em]">
+            <h3 className="text-primary1 font-[700] text-[1.6em]">
+              Our Commitment
+            </h3>
+
+            <p className="text-[1.0em]">
+              At CuriousKidz, we are committed to cultivating a generation of
+              young learners who possess a genuine love for knowledge. By
+              providing an entertaining and insightful approach to learning, we
+              aim to inspire students to reach for the stars and pursue advanced
+              disciplines in higher education.
+            </p>
+
+            <p className="text-[1.0em]">
+              Join us on this thrilling journey of discovery and growth.
+              Together, let's unlock the potential within each young mind,
+              nurturing curious minds that will shape the future of our world.
+            </p>
+            <div className=" block flexsm gap-3 text-[0.8em] font-[700] sm:items-start pt-[1em] sm:w-full flex-wrap sm:hidden">
+              <Button
+                className={
+                  "flex items-center gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 flex-grow justify-center"
+                }
+              >
+                Enroll now <FaArrowRight />
+              </Button>
+
+              <Button
+                className={
+                  "flex items-center gap-1 rounded-full px-[2.5em] py-[0.9em] text-primary1 bg-transparent border-primary1 border-2 flex-grow justify-center"
+                }
+              >
+                Login as a guest
+              </Button>
+            </div>
+          </div>
+        </PictureTextDivider>
+        <div className="hidden sm:block w-full sm:px-xpadding">
+          <div className="flexsm gap-3 text-[0.8em] font-[700] sm:items-start pt-[1em] sm:w-full flex-wrap bg-[#FAFAFA] pb-[5em]">
+            <Button
+              className={
+                "flex items-center gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 flex-grow justify-center"
+              }
+            >
+              Enroll now <FaArrowRight />
+            </Button>
+
+            <Button
+              className={
+                "flex items-center gap-1 rounded-full px-[2.5em] py-[0.9em] text-primary1 bg-transparent border-primary1 border-2 flex-grow justify-center"
+              }
+            >
+              Login as a guest
+            </Button>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Components/Price.jsx
+++ src/Components/Price.jsx
@@ -1,12 +1,17 @@
 import NavBar from "@/AtomicComponents/NavBar";
 import Footer from "@/AtomicComponents/Footer";
+import { ArrowRightOutline } from "heroicons-react";
+import Button from "@/AtomicComponents/Button";
+import Link from "next/link";
+import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa";
 
 const Price = () => {
   return (
     <>
       <NavBar active={3} background={"primary2"} />
       <div className="cflexss gap-[2em] w-full px-xpadding pt-[5em] pb-[3em] text-[0.9rem] font-[400]">
-        <div className="w-full cflexsm gap-[1em] text-center">
+        <div className="w-full cflexsm gap-[1em] text-center pb-[6em] sm:pb-0">
           <p>Pricing That Supports Learning Adventures</p>
           <h1 className="font-[700] text-[1.5rem] max-w-[25em]">
             Welcome to Curious Kids' Pricing Page! We believe in making quality
@@ -17,6 +22,74 @@ const Price = () => {
             learners and educational institutions, ensuring that the journey of
             curiosity and learning is open to everyone.
           </p>
+          <div className="flexss sm1:justify-center gap-[1em] font-[700] text-[0.8rem] flex-wrap sm1:w-full">
+            <Link href="/signup" className="sm:flex flex-grow">
+              <Button className="border-[0.2em] border-primary1 bg-primary1 text-white flexmm gap-[0.5em] px-[2.5em] py-[1em] rounded-full sm:w-full">
+                <p>Enroll now</p> <ArrowRightOutline size="1em" />
+              </Button>
+            </Link>
+            <Link href="/" className="sm:flex flex-grow">
+              <Button className="border border-[0.2em] border-primary1 text-primary1 flexmm gap-[0.5em] px-[2.5em] py-[1em] rounded-full sm:w-full">
+                <p>Login as a guest</p>
+              </Button>
+            </Link>
+          </div>
+        </div>
+        <div className="flexbm w-full gap-[1em] sm:gap-[2em] text-[0.9rem] font-[400] py-[3em] sm:py-[1em] flex-wrap sm:flex-col-reverse">
+          <div className="cflexms gap-[1em] max-w-[35em] h-full h-[40em] sm:h-full">
+            <p>For Individual Learners:</p>
+            <h1 className="font-[700] text-[1.5rem] text-primary1">
+              Unlock a World of Curiosity for Just ₦1500 per Term!
+            </h1>
+            <p>
+              Join the Curious Kids' community and embark on an adventure-filled
+              learning experience. For individual learners outside of school
+              settings, our pricing is as affordable as it is enriching.
+            </p>
+            <p>
+              For just ₦1500 per child per term, your young explorer gains
+              access to captivating educational videos, interactive challenges,
+              and a chance to earn exciting achievements.
+            </p>
+            <Button
+              className={
+                "flex items-center w-fit gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 sm:w-full sm:justify-center"
+              }
+            >
+              Enroll now <FaArrowRight />
+            </Button>
+          </div>
+          <div className="max-w-[35em] flexmm">
+            <Image src="unlock.svg" width={100} height={100} alt="unlock" />
+          </div>
+        </div>
+        <div className="flexbm w-full gap-[1em] sm:gap-[2em] text-[0.9rem] font-[400] py-[3em] sm:py-[1em] flex-wrap">
+          <div className="max-w-[35em] flexmm">
+            <Image src="flexible.svg" width={100} height={100} alt="flexible" />
+          </div>
+          <div className="cflexms gap-[1em] max-w-[35em] h-full h-[40em] sm:h-full">
+            <p>For Schools and Institutions:</p>
+            <h1 className="font-[700] text-[1.5rem] text-primary1">
+              Flexible Pricing According to Student Population
+            </h1>
+            <p>
+              We believe in supporting schools of all sizes. Our school pricing
+              is designed to accommodate various student populations. For school
+              users, the cost is as low as N1200 per child per term.
+            </p>
+            <p>
+              Schools can pay according to their specific student population
+              category, ensuring that every student has the opportunity to
+              embark on a journey of exploration and discovery.
+            </p>
+            <Button
+              className={
+                "flex items-center w-fit gap-1 rounded-full px-[2.5em] py-[1em] text-white bg-primary1 sm:w-full sm:justify-center"
+              }
+            >
+              Enroll now <FaArrowRight />
+            </Button>
+          </div>
         </div>
       </div>
       <Footer />
